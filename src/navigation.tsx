83c3e4e1fe95c1733962fa2c5becd13facc05285
lukas <mail@lukas-eisenhauer.de>
2023-02-22 10:07:02 +0100

    add user avatar outline

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,24 +1,26 @@
-import {Navbar} from '@nextui-org/react'
-import {useGlobalContext} from '@/context'
+import { Navbar, Dropdown, Avatar, Text } from '@nextui-org/react'
+import { useGlobalContext } from '@/context'
+import { useState } from 'react'
 
-function CustomLink({ target, children}) {
+export function Navigation({pages, enforceLogin, setUserState}) {
   const {context, setContext} = useGlobalContext()
-  const path = context.router.pathname
-  if (path == target || path == target + "/") {
+  const [avatarSource, setAvatarSource ] = useState('')
+  function CustomLink({ target, children}) {
+    const path = context.router.pathname
+    if (path == target || path == target + "/") {
+      return (
+        <Navbar.Link isActive href={target}>
+          {children}
+        </Navbar.Link>
+      )
+    }
     return (
-      <Navbar.Link isActive href={target}>
+      <Navbar.Link href={target}>
         {children}
       </Navbar.Link>
     )
   }
-  return (
-    <Navbar.Link href={target}>
-      {children}
-    </Navbar.Link>
-  )
-}
 
-export function Navigation({pages, enforceLogin, setUserState}) {
   return (
     <Navbar variant="floating" isBordered>
       <Navbar.Content variant="highlight-rounded">
@@ -26,6 +28,26 @@ export function Navigation({pages, enforceLogin, setUserState}) {
         <CustomLink target={page.target} key={page.target}>
           {page.name}
         </CustomLink>)}
+      </Navbar.Content>
+      <Navbar.Content css={{ w: "12%", jc: "flex-end"}}>
+        <Dropdown placement="bottom-right">
+          <Navbar.Item>
+            <Dropdown.Trigger>
+              <Avatar bordered as="button" color="secondary" size="md" text={context.username} src={avatarSource}/>
+            </Dropdown.Trigger>
+          </Navbar.Item>
+          <Dropdown.Menu aria-label="User menu actions" color="secondary">
+            <Dropdown.Item key="profile">
+              Signed in as {context.username}
+            </Dropdown.Item>
+            <Dropdown.Item key="settings" withDivider>
+              Settings
+            </Dropdown.Item>
+            <Dropdown.Item key="logout" withDivider color="error">
+              Log Out
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </Navbar.Content>
     </Navbar>
   )
