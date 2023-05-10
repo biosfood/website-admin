ceca2ae2de48799a57383632cfab0656e871ae56
lukas <mail@lukas-eisenhauer.de>
2023-05-10 10:20:08 +0200

    add support for rendered markdown cards (use with regular html tags)

--- src/RenderPage.tsx
+++ src/RenderPage.tsx
@@ -1,14 +1,17 @@
 import ReactMarkdown from 'react-markdown'
 import { useEffect, useState, ReactNode } from 'react'
 import Link from 'next/link'
+import rehypeRaw from 'rehype-raw'
+import { Card } from '@nextui-org/react';
 
 export default function RenderPage({children}: {children: ReactNode}) {
   return (
   <ReactMarkdown
     components={{
       a: ({node, href, ...props}: {node: any, href?: string}) => {
-        return <Link {...props} href={href!}/>}
-    }}>
+        return <Link {...props} href={href!}/>},
+        card: ({node, ...props}: {node: any}) => <Card><Card.Body {...props}/></Card>}}
+    rehypePlugins={[rehypeRaw]}>
     {children as string}
   </ReactMarkdown>)
 }
