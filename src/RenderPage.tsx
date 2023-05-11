eb290c5f7ecf710823c41d0e8fdc891d746acdaf
lukas <mail@lukas-eisenhauer.de>
2023-05-11 11:12:12 +0200

    add github-style markdown tables

--- src/RenderPage.tsx
+++ src/RenderPage.tsx
@@ -2,16 +2,40 @@ import ReactMarkdown from 'react-markdown'
 import { useEffect, useState, ReactNode } from 'react'
 import Link from 'next/link'
 import rehypeRaw from 'rehype-raw'
-import { Card } from '@nextui-org/react';
+import remarkGfm from 'remark-gfm'
+import { Card, Table } from '@nextui-org/react';
 
 export default function RenderPage({children}: {children: ReactNode}) {
   return (
   <ReactMarkdown
     components={{
       a: ({node, href, ...props}: {node: any, href?: string}) => {
-        return <Link {...props} href={href!}/>},
-        card: ({node, ...props}: {node: any}) => <Card><Card.Body {...props}/></Card>}}
-    rehypePlugins={[rehypeRaw]}>
+        return <Link {...props} href={href!}/>
+      },
+      card: ({node, ...props}: {node: any}) => <Card><Card.Body {...props}/></Card>,
+      table: ({node, children, ...props}: {node: any, children: ReactNode}) => {
+        if (!children) {
+          return null
+        }
+        const header = <Table.Header>
+          {(children as Array<{props: {children: Array<{props: {children: Array<any>}}>}}>)[0].props.children[0].props.children.map(
+              (child: {props: {children: any[]}}) => <Table.Column key={child.props.children[0]}>{child.props.children[0]}</Table.Column>)
+          }
+        </Table.Header>
+        const body = <Table.Body>
+          {(children as Array<{props: {children: Array<any>}}>)[1].props.children.map(
+              (child: {props: {children: Array<any>}}, index: number) => <Table.Row key={index}>
+            {child.props.children.map((child: {props: {children: any}}, index: number) => <Table.Cell key={index}>{child.props.children}</Table.Cell>)}
+          </Table.Row>)}
+        </Table.Body>
+        return <Table>
+          {header}
+          {body}
+        </Table>
+      }
+    } as {a: any,  table: any}}
+    rehypePlugins={[rehypeRaw]}
+    remarkPlugins={[remarkGfm]}>
     {children as string}
   </ReactMarkdown>)
 }
